4fba97b36de4412a4328732e7d5d4a3aa612d3bb
carlos velazquez <carlosvbenitez@gmail.com>
2019-02-03 17:31:00 +0100

    44

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+     
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Descripcion { get; set; }
+
+
+        public Decimal Price { get; set; }
+
+
+        public bool IsAvailable { get; set; }
+
+
+        public DateTime PublishOn { get; set; }
+    }
+}
